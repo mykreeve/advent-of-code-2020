a9934644583178560408a52345b829a0bee0716d
michael <myk.reeve@gmail.com>
2020-12-02 07:18:12 +0000

    day 2 done

--- day1.py
+++ day1.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 filename="input/day1input.txt"
 file=open(filename,"r")
@@ -7,19 +8,21 @@ numbers = []
 for f in file:
     numbers.append(int(f.replace('\n','')))
 
-
+now = datetime.now()
 for a in range(len(file)):
     for b in range(a,len(file)):
         if a == b:
             continue
         if (numbers[a]+numbers[b] == 2020):
+            done = datetime.now()
             print ("Answer to part 1:", numbers[a] * numbers[b], '(', numbers [a], ',', numbers[b], ')')
+            print ("Time taken:", done - now)
             break
     else:
         continue
     break
 
-
+now = datetime.now()
 for a in range(len(file)):
     for b in range(a,len(file)):
         if (a == b or (a+b) > 2020):
@@ -28,7 +31,9 @@ for a in range(len(file)):
             if (a == b or a == c or b == c):
                 continue
             if (numbers[a]+numbers[b]+numbers[c] == 2020):
+                done = datetime.now()
                 print ("Answer to part 2:", numbers[a] * numbers[b] * numbers[c], '(', numbers [a], ',', numbers[b], ',', numbers[c], ')')
+                print ("Time taken:", done - now)
                 break
         else:
             continue
---
+++ day2.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+filename="input/day2input.txt"
+file=open(filename,"r")
+file=file.readlines()
+
+now = datetime.now()
+goodpass = 0
+for f in file:
+    f = f.split(':')
+    rules = f[0].strip()
+    rules = rules.split(' ')
+    minval = int(rules[0].split('-')[0])
+    maxval = int(rules[0].split('-')[1])
+    char = rules[1]
+    password = f[1].strip()
+    counter = 0
+    for p in password:
+        if p == char:
+            counter += 1
+    if minval <= counter and maxval >= counter:
+        goodpass += 1
+done = datetime.now()
+print ("Answer for part one:", goodpass)
+print ("Time taken:", done - now)
+
+now = datetime.now()
+goodpass = 0
+for f in file:
+    f = f.split(':')
+    rules = f[0].strip()
+    rules = rules.split(' ')
+    minpos = int(rules[0].split('-')[0])
+    maxpos = int(rules[0].split('-')[1])
+    char = rules[1]
+    password = f[1].strip()
+    counter = 0
+    if password[minpos-1] == char:
+            counter += 1
+    if password[maxpos-1] == char:
+            counter += 1
+    if counter == 1:
+        goodpass += 1
+done = datetime.now()
+print ("Answer for part two:", goodpass)
+print ("Time taken:", done - now)
