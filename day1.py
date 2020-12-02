920a290e4d579f8a3691537cfae0b52dbe452ea2
michael <myk.reeve@gmail.com>
2020-12-02 23:34:41 +0000

    improving day 1 speed

--- day1.py
+++ day1.py
@@ -10,34 +10,22 @@ for f in file:
 
 now = datetime.now()
 for a in range(len(file)):
-    for b in range(a,len(file)):
-        if a == b:
-            continue
-        if (numbers[a]+numbers[b] == 2020):
-            done = datetime.now()
-            print ("Answer to part 1:", numbers[a] * numbers[b], '(', numbers [a], ',', numbers[b], ')')
-            print ("Time taken:", done - now)
-            break
-    else:
-        continue
-    break
+    if (2020 - numbers[a]) in numbers:
+        done = datetime.now()
+        print ("Answer to part 1:", numbers[a] * (2020 - numbers[a]), '(', numbers [a], ',', (2020 - numbers[a]), ')')
+        print ("Time taken:", done - now)
+        break
 
 now = datetime.now()
 for a in range(len(file)):
     for b in range(a,len(file)):
         if (a == b or (a+b) > 2020):
             continue
-        for c in range(b,len(file)):
-            if (a == b or a == c or b == c):
-                continue
-            if (numbers[a]+numbers[b]+numbers[c] == 2020):
-                done = datetime.now()
-                print ("Answer to part 2:", numbers[a] * numbers[b] * numbers[c], '(', numbers [a], ',', numbers[b], ',', numbers[c], ')')
-                print ("Time taken:", done - now)
-                break
-        else:
-            continue
-        break
+        if (2020 - numbers[a] - numbers[b]) in numbers:
+            done = datetime.now()
+            print ("Answer to part 2:", numbers[a] * numbers[b] * (2020 - numbers[a] - numbers[b]), '(', numbers [a], ',', numbers[b], ',', (2020 - numbers[a] - numbers[b]), ')')
+            print ("Time taken:", done - now)
+            break
     else:
         continue
     break
---
+++ day3.py
@@ -0,0 +1,6 @@
+filename="input/day2input.txt"
+file=open(filename,"r")
+file=file.readlines()
+
+for f in file:
+    print(f)
