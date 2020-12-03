1005bfbd59646145c4cfbe85f4eb033e066a6cd5
michael <myk.reeve@gmail.com>
2020-12-03 07:06:52 +0000

    day 3 done

--- day3.py
+++ day3.py
@@ -1,6 +1,44 @@
-filename="input/day2input.txt"
+from datetime import datetime
+
+filename="input/day3input.txt"
 file=open(filename,"r")
 file=file.readlines()
 
-for f in file:
-    print(f)
+grid = {}
+
+now = datetime.now()
+for n1,f in enumerate(file):
+    maxy = len(file)
+    for n2,p in enumerate(f):
+        maxx = n2+1
+        grid[(n2,n1)] = p
+
+pos = (0,0)
+crashes = [0,0,0,0,0]
+
+while pos[1] < maxy:
+    if grid[pos] == '#':
+        crashes[0] += 1
+    pos = ((pos[0] + 3)%maxx, pos[1] + 1)
+
+done = datetime.now()
+print ("Answer to part 1:", crashes[0])
+print ("Time taken:", done - now)
+
+now = datetime.now()
+alternatives = [(1,1),(5,1),(7,1),(1,2)]
+
+for n,a in enumerate(alternatives):
+    pos = (0,0)
+    while pos[1] < maxy:
+        if grid[pos] == '#':
+            crashes[n+1] += 1
+        pos = ((pos[0] + a[0])%maxx, pos[1] + a[1])
+
+multiply = 1
+for c in crashes:
+    multiply *= c
+
+done = datetime.now()
+print ("Answer to part 2:", multiply)
+print ("Time taken:", done - now)
