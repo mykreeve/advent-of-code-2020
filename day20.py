abefb83ac7bb47af4a99f5f5fd3dd6c5c49d70e9
michael <myk.reeve@gmail.com>
2020-12-21 15:22:02 +0000

    getting there

--- day20.py
+++ day20.py
@@ -143,27 +143,29 @@ for y in range(0,12):
     for x in range(0,12):
         print(mapped[(x,y)] , end= " ")
     print ('')
+print ("")
 
 oriented = {}
 
-# def print_diff_line_tile(tile):
-#     for t in tile:
-#         print ('           '+t)
+def print_diff_line_tile(tile):
+    for t in tile:
+        print ('           '+t)
+    print('\n')
 
-# def print_left_tile(tile1, tile2):
-#     for n in range(len(tile1)):
-#         print(tile1[n]+' '+tile2[n])
-#     print('\n')
+def print_left_tile(tile1, tile2):
+    for n in range(len(tile1)):
+        print(tile1[n]+' '+tile2[n])
+    print('\n')
 
-# def print_right_tile(tile1,tile2):
-#     for n in range(len(tile1)):
-#         print('           '+tile1[n]+' '+tile2[n])
-#     print('\n')
+def print_right_tile(tile1,tile2):
+    for n in range(len(tile1)):
+        print('           '+tile1[n]+' '+tile2[n])
+    print('\n')
 
-# def print_side_tiles(tile1,tile2,tile3):
-#     for n in range(len(tile1)):
-#         print(tile1[n],tile2[n],tile3[n])
-#     print('\n')
+def print_side_tiles(tile1,tile2,tile3):
+    for n in range(len(tile1)):
+        print(tile1[n],tile2[n],tile3[n])
+    print('\n')
 
 def get_sides_from_tile(tile):
     sides = []
@@ -204,6 +206,28 @@ def get_sides_from_ymirror_tile(tile):
     sides.append(othernextitem[::-1])
     return sides
 
+def get_sides_from_xymirror_tile(tile):
+    sides = []
+    sides.append(tile[len(tile)-1][::-1])
+    nextitem = ''
+    othernextitem = ''
+    for n in tile:
+        nextitem += n[-1:]
+        othernextitem += n[0]
+    sides.append(nextitem[::-1])
+    sides.append(tile[0][::-1])
+    sides.append(othernextitem[::-1])
+    return sides
+
+def rotate(tile):
+    new_tile = []
+    for y in range(len(tile)-1,-1,-1):
+        new_line = ''
+        for x in range(len(tile)):
+            new_line += tile[x][y]
+        new_tile.append(new_line)
+    return new_tile            
+
 while len(oriented) < 144:
     for y in range(0,12):
         for x in range(0,12):
@@ -213,22 +237,80 @@ while len(oriented) < 144:
                 opts = [get_sides_from_tile(t)]
                 opts.append(get_sides_from_xmirror_tile(t))
                 opts.append(get_sides_from_ymirror_tile(t))
+                if len(oriented) > 89:
+                    opts.append(get_sides_from_tile(rotate(t)))
+                if len(oriented) > 100:
+                    opts.append(get_sides_from_xmirror_tile(rotate(t)))
+                if len(oriented) > 109:
+                    opts.append(get_sides_from_ymirror_tile(rotate(t)))
 
+                fix = 0
                 for n,d in enumerate(directions):
                     if x+d[0] >= 0 and x+d[0] <= 11 and y+d[1] >= 0 and y+d[1] <= 11:
-                        neighbours[n] = tiles[mapped[(x+d[0],y+d[1])]]
+                        if (x+d[0],y+d[1]) in oriented:
+                            tempn = oriented[(x+d[0],y+d[1])]
+                            neighbours[n] = [get_sides_from_tile(tempn)[(n+2)%4]]
+                            fix += 1
+                        else:
+                            tempn = tiles[mapped[(x+d[0],y+d[1])]]
+                            neighbours[n] = get_sides_from_tile(tempn)
+                            for xn in get_sides_from_xmirror_tile(tempn):
+                                if xn not in neighbours[n]:
+                                    neighbours[n].append(xn)
+                            for xn in get_sides_from_ymirror_tile(tempn):
+                                if xn not in neighbours[n]:
+                                    neighbours[n].append(xn)
 
-                for n in neighbours:
-                    print(get_sides_from_tile(neighbours[n]))
+                if len(oriented) == 115:
+                    for yyy in range(12):
+                        for line in range(10):
+                            for xxxx in range(12):
+                                if (xxxx,yyy) in oriented:
+                                    print (oriented[(xxxx,yyy)][line],end=" ")
+                                else:
+                                    print ("          ", end=" ")
+                            print('')
+                    print(opts)
+                    print(x,y,neighbours)
+                    input('--')
 
-                # if 0 in neighbours:
-                #     print_diff_line_tile(neighbours[0])
-                # if 1 in neighbours and 3 not in neighbours:
-                #     print_right_tile(tiles[mapped[(x,y)]],neighbours[1])
-                # if 3 in neighbours and 1 not in neighbours:
-                #     print_left_tile(neighbours[3],tiles[mapped[(x,y)]])
-                # if 3 in neighbours and 1 in neighbours:
-                #     print_side_tiles(neighbours[3],tiles[mapped[(x,y)]],neighbours[1])
-                # if 2 in neighbours:
-                #     print_diff_line_tile(neighbours[2])
-                exit()
+                insert_options = []
+                if fix >= 1 or (x,y) == (0,0):
+                    for v,o in enumerate(opts):
+                        for orient in range(4):
+                            match = 0
+                            for n in neighbours:
+                                if o[(orient+n)%4] in neighbours[n]:
+                                    match += 1
+                            if match == len(neighbours):
+                                changed = copy.deepcopy(t)
+                                # print ((x,y), v, orient, "WOOP")
+                                if v == 0:
+                                    print('')
+                                elif v == 1:
+                                    temp2 = []
+                                    for xx in changed:
+                                        temp2.append(xx[::-1])
+                                    changed = temp2
+                                elif v == 2:
+                                    changed = changed[::-1]
+                                if orient > 0:
+                                    for rots in range(orient):
+                                        changed = rotate(changed)
+                                if (x,y) not in oriented:
+                                    if changed not in insert_options:
+                                        insert_options.append(changed)
+                if len(insert_options) == 1:
+                    oriented[(x,y)] = insert_options[0]
+
+    # for y in range(12):
+    #     for line in range(10):
+    #         for x in range(12):
+    #             if (x,y) in oriented:
+    #                 print (oriented[(x,y)][line],end=" ")
+    #             else:
+    #                 print ("          ", end=" ")
+    #         print('')
+
+    prompt = str(len(oriented)) +' --'
+    input(prompt)
